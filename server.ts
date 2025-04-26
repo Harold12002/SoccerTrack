87f447df696c85f29917431f7a931fb55f342ab4
Harold12002~ <chipuriroh@gmail.com>
2025-04-26 04:34:54 +0200

    Added middleware for frontend connection

--- server.ts
+++ server.ts
@@ -1,10 +1,18 @@
-import { Application } from "./src/imports.ts";
+import { Application, oakCors } from "./src/imports.ts";
 import routes from "./src/routes/routes.ts";
 
 const PORT = Number(Deno.env.get("PORT")) || 8000;
 
 // Setting up Oak application
 const app = new Application();
+
+// Enable CORS for all routes
+app.use(oakCors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+}));
 
 // Router middleware
 app.use(routes.routes());
